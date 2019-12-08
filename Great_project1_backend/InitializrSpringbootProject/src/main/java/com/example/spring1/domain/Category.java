78d7d0c44dccf044a0c33290d21fc27c4a81a7f4
erick lucio <ericklucio-suv@hotmail.com>
2019-12-08 14:50:45 -0300

    retornando json

---
+++ Great_project1_backend/InitializrSpringbootProject/src/main/java/com/example/spring1/domain/Category.java
@@ -0,0 +1,68 @@
+
+package com.example.spring1.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Category implements Serializable{
+   private static final long SerialVersionUID = 1L; 
+   private Integer id;
+   private String name;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+   
+   
+}
--- Great_project1_backend/InitializrSpringbootProject/src/main/java/com/example/spring1/resources/CategoryResource.java
+++ Great_project1_backend/InitializrSpringbootProject/src/main/java/com/example/spring1/resources/CategoryResource.java
@@ -1,6 +1,9 @@
 
 package com.example.spring1.resources;
 
+import com.example.spring1.domain.Category;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +12,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value="/erick")
 public class CategoryResource {
     @RequestMapping(method=RequestMethod.GET)
-    public String categoria(){
-        return "Micael e viadao Mateus e mais!S2";
+    public List<List> categoria(){
+        Category cat1 = new Category(0,"erick");
+        Category cat2 = new Category(1, "e");
+        Category cat3 = new Category(2,"bolado");
+        Category cat4 = new Category(3, "demais");
+        Category cat5 = new Category(4,"se ta");
+        Category cat6 = new Category(5, "e");
+        Category cat7 = new Category(6,"doido");
+        Category cat8 = new Category(7, "fon");
+        Category cat9 = new Category(8,"nois q voa");
+        Category cat10 = new Category(9, "#PAS");
+        
+        List<Category> lista1= new ArrayList<Category>();
+        
+        lista1.add(cat1);
+        lista1.add(cat2);
+        lista1.add(cat3);
+        lista1.add(cat4);
+        lista1.add(cat5);
+        lista1.add(cat6);
+        lista1.add(cat7);
+        lista1.add(cat8);
+        lista1.add(cat9);
+        lista1.add(cat10);
+        List<List> categorylist= new ArrayList<List>();
+        categorylist.add(lista1);
+        categorylist.add(lista1);
+        categorylist.add(lista1);
+        categorylist.add(lista1);
+        return categorylist;
     }
     
 }
