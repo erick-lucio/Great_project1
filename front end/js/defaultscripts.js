1c4712c3595f7891b081e9b63a6f0a78aa08c14c
erick lucio <ericklucio-suv@hotmail.com>
2020-01-13 22:57:47 -0300

    adicionado for q acessa todos os objetos do json no front end

--- front end/js/defaultscripts.js
+++ front end/js/defaultscripts.js
@@ -28,7 +28,7 @@ function getrequest(){
     $.get( "http://localhost:8080/erick", function(data) {
         
         let obj = JSON.parse(data);
-        console.log(obj);
+        //console.log(obj);
        
         //console.log(obj['name']);
         dealwithjson(obj);
@@ -38,12 +38,32 @@ function getrequest(){
 }
 
 function dealwithjson(jsonObj){
-    var countKey = Object.keys(jsonObj).length;
-    console.log( countKey) ;
+    //var countKey = Object.keys(jsonObj.name).length;
+   // console.log( countKey);
      //console.log(jsonObj["name"]+" id: "+jsonObj['id']);
-     //console.log(jsonObj["galaxyList"][0]["solarSystem"][0]["planet"][0]["name"]);
+    // console.log(jsonObj.galaxyList[0].solarSystem[0].planet.length);
     //console.log(jsonObj["galaxyList"][0]["solarSystem"][0]["star"][0]["name"]);
     //console.log(jsonObj["galaxyList"][0]["solarSystem"][0]["blackHole"][0]["name"]);
+    
+    console.log(jsonObj.name);
+    for(let i = 0;i<jsonObj.galaxyList.length;i++){
+        console.log(jsonObj.galaxyList[i].name);
+        for(let j = 0;j<jsonObj.galaxyList[i].solarSystem.length;j++){
+            console.log(jsonObj.galaxyList[i].solarSystem[j].name);
+            for( let p=0;p<jsonObj.galaxyList[i].solarSystem[j].planet.length;p++){
+                 console.log(jsonObj.galaxyList[i].solarSystem[j].planet[p].name);
+            }
+            for( let s=0;s<jsonObj.galaxyList[i].solarSystem[j].star.length;s++){
+                 console.log(jsonObj.galaxyList[i].solarSystem[j].star[s].name);
+            }
+            for( let b=0;b<jsonObj.galaxyList[i].solarSystem[j].blackHole.length;b++){
+                 console.log(jsonObj.galaxyList[i].solarSystem[j].blackHole[b].name);
+            }
+            
+        }
+        
+        
+    }
      //$( ".second-main" )
     // .append( document.createTextNode( "!!!" ) )
     // .css( "color", "red" );  
