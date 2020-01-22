0558163313dc673c3dcc14569c7d26b0e5cc6d98
erick lucio <ericklucio-suv@hotmail.com>
2020-01-21 23:10:10 -0300

    mudei umas coisinhas so no react nada importante

---
+++ front end/react-app/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Post from './Post';
+
+export default class App extends React.Component{
+  render(){
+    return(
+     <div>
+         <h2>Erick lindao</h2>
+         <Post title="GARAI"/>
+      </div>
+    );
+  }
+}
