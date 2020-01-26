b02ab8908b0812e770a504b73e0b397bc46f3f95
erick lucio <ericklucio-suv@hotmail.com>
2020-01-26 16:59:19 -0300

    esta recebenbdo da api e exibindo no html,um pouquinho de progresso hj

---
+++ front end/react-app/src/components/Canvas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Canvas extends React.Component{
+    render(){
+        return(
+            <div className="w3-container canvasDiv">
+                <Canvas>
+                    
+                </Canvas>
+            </div>
+        );
+    }
+}
