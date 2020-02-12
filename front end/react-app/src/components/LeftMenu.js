bb78591ffc0fae9ecc16fb74c4e08d2b4cb22dc5
erick lucio <ericklucio-suv@hotmail.com>
2020-02-11 23:02:32 -0300

    tirei uns bang inutil

--- front end/react-app/src/components/LeftMenu.js
+++ front end/react-app/src/components/LeftMenu.js
@@ -4,8 +4,13 @@ export default class LeftMenu extends React.Component{
     render(){
         return(
             <div className="w3-container LeftMenu">
+                <div>
+                    <p>Please Login</p>
+                    <submit>Email</submit>
+                    <submit>Password</submit>
+                    <button>Login</button>
+                </div>
                
-                Left Menu
 
             </div>
         );
